4b1fd322d1e3c65abc4b67b5b122db9852f60b92
Austin Reimer <austin.d.reimer@gmail.com>
2020-03-23 15:25:10 -0500

    Set up the view and css

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,9 +3,16 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def alphabetized
+    @customers = Customer.all.order(:full_name)
+  end
+
   def show
     @customer = Customer.find(params[:id])
   end
 
+  def missing_email
+    @customers = Customer.where(email: [nil, ""])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources 'customers', only: %i[index show]
+  get 'customers/missing_email', to: 'customers#missing_email', as: 'missing_email'
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
+  resources :customers, only: %i[index show]
+
+
+  root to: 'customers#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
